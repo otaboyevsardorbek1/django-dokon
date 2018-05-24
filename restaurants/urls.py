b4b7707c3b3466a2c523a3aeefaa9ef6637e787b
voronov007 <voronov.a.g@gmail.com>
2018-05-24 19:01:59 +0300

    url fix

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'restaurant/(?P<restaurant_id>\d+)/?$', views.get_restaurant, name='get_restaurant'),
-    url(r'restaurant/?$', views.delete_restaurant, name='delete_restaurant')
+    url(r'^restaurant/(?P<restaurant_id>\d+)/?$', views.get_restaurant, name='get_restaurant'),
+    url(r'^restaurant/?$', views.delete_restaurant, name='delete_restaurant')
 ]
